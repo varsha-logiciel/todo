a29ffb5e709f1c36bd1016ced0fbf9bc8a21f3af
varsha-logiciel <87015166+varsha-logiciel@users.noreply.github.com>
2021-07-07 06:21:06 -0700

    initial commit

---
+++ www/js/controller.js
@@ -0,0 +1,48 @@
+(function(){
+	'use strict';	
+		var DemoCtrl = function($scope, $ionicActionSheet, $ionicBackdrop, $timeout) {
+			$scope.show = function() {
+
+				// Show the action sheet
+				var hideSheet = $ionicActionSheet.show({
+					buttons: [
+						{ text: '<b>Share</b> This' },
+						{ text: 'Move' }
+					],
+					destructiveText: 'Delete',
+					titleText: 'Modify your album',
+					cancelText: 'Cancel',
+					cancel: function() {
+							 // add cancel code..
+						 },
+					buttonClicked: function(index) {
+						return true;
+					}
+				});
+		 
+			};
+
+			//Show a backdrop for one second
+			$scope.action = function() {
+				$ionicBackdrop.retain();
+				$timeout(function() {
+					$ionicBackdrop.release();
+				}, 1000);
+			};
+
+			// Execute action on backdrop disappearing
+			$scope.$on('backdrop.hidden', function() {
+				// Execute action
+			});
+
+			// Execute action on backdrop appearing
+			$scope.$on('backdrop.shown', function() {
+				// Execute action
+			});
+		}
+
+		DemoCtrl.$inject = ['$scope', '$ionicActionSheet', '$ionicBackdrop', '$timeout'];
+		angular
+			.module('starter')
+			.controller('MainCtrl',DemoCtrl);
+	})();
