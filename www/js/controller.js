460fe2593c65812f65ead703dfa8d1ce2c7f7bff
varsha-logiciel <87015166+varsha-logiciel@users.noreply.github.com>
2021-07-12 18:48:24 -0700

    javascript components
    My today’s Learning (javascript Components) includes:-ion-popup
    ion-popover
    model
    ion-tabs
    ion-tab
    ion-degitial tabs

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,6 +1,6 @@
 (function(){
 	'use strict';	
-		var DemoCtrl = function($scope, $ionicActionSheet, $ionicBackdrop, $timeout) {
+		var DemoCtrl = function($scope, $ionicActionSheet, $ionicBackdrop, $timeout,$ionicPopup) {
 			$scope.show = function() {
 
 				// Show the action sheet
@@ -21,7 +21,7 @@
 				});
 		 
 			};
-			
+
 			//Show a backdrop for one second
 			$scope.action = function() {
 				$ionicBackdrop.retain();
@@ -39,9 +39,43 @@
 			$scope.$on('backdrop.shown', function() {
 				// Execute action
 			});
+		$scope.showPopup = function() {
+			$scope.data = {};
+		  
+			// An elaborate, custom popup
+			var myPopup = $ionicPopup.show({
+			  template: '<input type="password" ng-model="data.wifi">',
+			  title: 'Enter Wi-Fi Password',
+			  subTitle: 'Please use normal things',
+			  scope: $scope,
+			  buttons: [
+				{ text: 'Cancel' },
+				{
+				  text: '<b>Save</b>',
+				  type: 'button-positive',
+				  onTap: function(e) {
+					if (!$scope.data.wifi) {
+					  //don't allow the user to close unless he enters wifi password
+					  e.preventDefault();
+					} else {
+					  return $scope.data.wifi;
+					}
+				  }
+				}
+			  ]
+			});
+		  
+			myPopup.then(function(res) {
+			  console.log('Tapped!', res);
+			});
+		  
+			$timeout(function() {
+			   myPopup.close(); //close the popup after 3 seconds for some reason
+			}, 3000);
+		   }
 		}
 
-		DemoCtrl.$inject = ['$scope', '$ionicActionSheet', '$ionicBackdrop', '$timeout'];
+		DemoCtrl.$inject = ['$scope', '$ionicActionSheet', '$ionicBackdrop', '$timeout','$ionicPopup'];
 		angular
 			.module('starter')
 			.controller('MainCtrl',DemoCtrl);
