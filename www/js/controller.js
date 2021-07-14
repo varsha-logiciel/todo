624f5497e908dee58af0a6bcf011f642bfcda865
varsha-logiciel <87015166+varsha-logiciel@users.noreply.github.com>
2021-07-13 18:37:09 -0700

    Javascript concepts;-
    Here is a detail about my today's leanings about Javascript components:-
    Models
    Popover
    spinners
    tab
    slides and pics set in slide

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,6 +1,6 @@
 (function(){
 	'use strict';	
-		var DemoCtrl = function($scope, $ionicActionSheet, $ionicBackdrop, $timeout,$ionicPopup) {
+		var DemoCtrl = function($scope, $ionicActionSheet, $ionicBackdrop, $timeout, $ionicPopup, $ionicPopover, $ionicModal) {
 			$scope.show = function() {
 
 				// Show the action sheet
@@ -39,43 +39,96 @@
 			$scope.$on('backdrop.shown', function() {
 				// Execute action
 			});
-		$scope.showPopup = function() {
-			$scope.data = {};
-		  
-			// An elaborate, custom popup
-			var myPopup = $ionicPopup.show({
-			  template: '<input type="password" ng-model="data.wifi">',
-			  title: 'Enter Wi-Fi Password',
-			  subTitle: 'Please use normal things',
-			  scope: $scope,
-			  buttons: [
-				{ text: 'Cancel' },
-				{
-				  text: '<b>Save</b>',
-				  type: 'button-positive',
-				  onTap: function(e) {
-					if (!$scope.data.wifi) {
-					  //don't allow the user to close unless he enters wifi password
-					  e.preventDefault();
-					} else {
-					  return $scope.data.wifi;
-					}
-				  }
-				}
-			  ]
-			});
-		  
-			myPopup.then(function(res) {
-			  console.log('Tapped!', res);
-			});
-		  
-			$timeout(function() {
-			   myPopup.close(); //close the popup after 3 seconds for some reason
-			}, 3000);
+
+			$scope.showPopup = function() {
+				$scope.data = {};
+			
+				// An elaborate, custom popup
+				var myPopup = $ionicPopup.show({
+					template: '<input type="password" ng-model="data.wifi">',
+					title: 'Enter Wi-Fi Password',
+					subTitle: 'Please use normal things',
+					scope: $scope,
+					buttons: [
+						{ text: 'Cancel' },
+						{
+							text: '<b>Save</b>',
+							type: 'button-positive',
+							onTap: function(e) {
+								if (!$scope.data.wifi) {
+								//don't allow the user to close unless he enters wifi password
+									e.preventDefault();
+								} else {
+									return $scope.data.wifi;
+								}
+							}
+						}
+					]
+				});
+			
+				myPopup.then(function(res) {
+					console.log('Tapped!', res);
+				});
+			
+				$timeout(function() {
+					myPopup.close(); //close the popup after 3 seconds for some reason
+				}, 3000);
 		   }
+
+			$ionicPopover.fromTemplateUrl('templates/my-popover.html', {
+				scope: $scope
+			}).then(function(popover) {
+				$scope.popover = popover;
+			});
+
+
+			$scope.openPopover = function($event) {
+				$scope.popover.show($event);
+			};
+			$scope.closePopover = function() {
+				$scope.popover.hide();
+			};
+			//Cleanup the popover when we're done with it!
+			$scope.$on('$destroy', function() {
+				$scope.popover.remove();
+			});
+			// Execute action on hidden popover
+			$scope.$on('popover.hidden', function() {
+				// Execute action
+			});
+			// Execute action on remove popover
+			$scope.$on('popover.removed', function() {
+				// Execute action
+			});
+			$ionicModal.fromTemplateUrl('templates/my-modal.html', {
+				scope: $scope,
+				animation: 'slide-in-up'
+			  }).then(function(modal) {
+				$scope.modal = modal;
+			  });
+			  $scope.openModal = function() {
+				$scope.modal.show();
+			  };
+			  $scope.closeModal = function() {
+				$scope.modal.hide();
+			  };
+			  // Cleanup the modal when we're done with it!
+			  $scope.$on('$destroy', function() {
+				$scope.modal.remove();
+			  });
+			  // Execute action on hide modal
+			  $scope.$on('modal.hidden', function() {
+				// Execute action
+			  });
+			  // Execute action on remove modal
+			  $scope.$on('modal.removed', function() {
+				// Execute action
+			  });
+			  
+			   
 		}
 
-		DemoCtrl.$inject = ['$scope', '$ionicActionSheet', '$ionicBackdrop', '$timeout','$ionicPopup'];
+		DemoCtrl.$inject = ['$scope', '$ionicActionSheet', '$ionicBackdrop', '$timeout','$ionicPopup', '$ionicPopover', '$ionicModal'];
 		angular
 			.module('starter')
 			.controller('MainCtrl',DemoCtrl);
